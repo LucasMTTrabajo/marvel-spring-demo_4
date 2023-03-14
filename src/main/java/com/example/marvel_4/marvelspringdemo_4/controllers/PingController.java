1d983aeac990bbab2a4d21813331dbe720b6a9bc
LucasMT <lucasmttrabajo@gmail.com>
2023-03-13 21:26:08 -0300

    Initial scaffolding

---
+++ src/main/java/com/example/marvel_4/marvelspringdemo_4/controllers/PingController.java
@@ -0,0 +1,13 @@
+package com.example.marvel_4.marvelspringdemo_4.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Pong!";
+    }
+}
---
+++ src/test/java/com/example/marvel_4/marvelspringdemo_4/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marvel_4.marvelspringdemo_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
