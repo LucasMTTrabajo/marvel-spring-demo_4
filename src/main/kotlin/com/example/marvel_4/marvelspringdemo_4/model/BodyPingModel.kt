ce0454bab2db02b72e1c3d6bb6d8b9d206589e85
LucasMT <lucasmttrabajo@gmail.com>
2023-11-01 20:59:47 -0300

    Fix

--- src/main/kotlin/com/example/marvel_4/marvelspringdemo_4/model/BodyPingModel.kt
+++ src/main/kotlin/com/example/marvel_4/marvelspringdemo_4/model/BodyPingModel.kt
@@ -1,4 +1,4 @@
 package com.example.marvel_4.marvelspringdemo_4.model
 
 import org.springframework.boot.configurationprocessor.json.JSONObject
-data class BodyPingModel(val data : JSONObject?, val id: String? )
+data class BodyPingModel(val data : JSONObject?, val id : String?)
